70dc85c8ad80b70d5d735fff46242e0150ffa70c
Gamzat Rasulov <c0mrade3645@gmail.com>
2018-01-14 01:58:33 +0300

    add view create

---
+++ resources/views/notebooks/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.base')
+
+
+@section('content')
+<div class="container"
+    <h1>Create Notebook</h1>
+
+    <form action="">
+        <div class="form-group">
+            <label for="name">Notebook Name</label>
+            <input class="form-control" type="text" name="name"></input>
+        </div>
+        <input class="btn btn-primary" type="submit" name="" value="Done"></input>
+    </form>
+</div>
+@endsection
