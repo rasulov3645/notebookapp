2b652afac531079221f8e032bcc5a29087a1b45b
Gamzat Rasulov <c0mrade3645@gmail.com>
2018-01-14 01:57:28 +0300

    edit view index

--- resources/views/notebooks/index.blade.php
+++ resources/views/notebooks/index.blade.php
@@ -1,29 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-            <title>
-                NoteBook App
-            </title>
-            <link href="dist/css/main.css" rel="stylesheet">
-                <link href="dist/css/bootstrap.css" rel="stylesheet">
-                </link>
-            </link>
-        </meta>
-    </head>
-    <body>
-        <div class="container-fluid">
-            <nav class="navbar navbar-dark bg-primary">
-                <button aria-controls="navbar-header" class="navbar-toggler hidden-sm-up" data-target="#navbar-header" data-toggle="collapse" type="button">
-                    ☰
-                </button>
-                <div class="collapse navbar-toggleable-xs" id="navbar-header">
-                    <a class="navbar-brand" href="#">
-                        NoteBook App
-                    </a>
-                </div>
-            </nav>
-            <!-- /navbar -->
+@extends('layouts.base')
+
+
+@section('content')
             <!-- Main component for call to action -->
             <div class="container text-center">
                 <!-- heading -->
@@ -67,14 +45,7 @@
                         </div>
                     </div>
                 </div>
-
                 @endforeach
             </div>
             <!-- /container -->
-            <script src="dist/js/jquery3.min.js">
-            </script>
-            <script src="dist/js/bootstrap.js">
-            </script>
-        </div>
-    </body>
-</html>
+@endsection
