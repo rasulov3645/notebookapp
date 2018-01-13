9608219d7fa571776dbf13949de0957512e1c9f9
Gamzat Rasulov <c0mrade3645@gmail.com>
2018-01-14 01:44:31 +0300

    edit view frontpage

--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -1,24 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.base')
 
-<head>
-    <meta charset="UTF-8">
-    <title>NoteBook App</title>
-    <link href="dist/css/main.css" rel="stylesheet" >
-    <link href="dist/css/bootstrap.css" rel="stylesheet">
-</head>
-
-<body>
-    <div class="container-fluid">
-        <nav class="navbar  navbar-dark bg-primary">
-            <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#navbar-header" aria-controls="navbar-header">
-                &#9776;
-            </button>
-            <div class="collapse navbar-toggleable-xs" id="navbar-header">
-                <a class="navbar-brand" href="#">NoteBook App</a>
-
-        </nav>
-        <!-- /navbar -->
+@section('content')
         <!-- Main component for call to action -->
         <div class="jumbotron">
             <h1>Notebook</h1>
@@ -29,9 +11,4 @@
         </div>
     </div>
     <!-- /container -->
-
-    <script src="{{asset('dist/js/jquery3.min.js')}}"></script>
-    <script src="{{asset('dist/js/bootstrap.js')}}"></script>
-</body>
-
-</html>
+@endsection
